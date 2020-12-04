1f61047d87157002adc062409c20dd01b12bab1c
Sam Feig <spfeig@gmail.com>
2020-12-04 15:24:04 -0700

    Integrated changes to u and checked movement. Removed c_prob and cleaned up parts of EKF. TODO: Reduce number of obstacles

--- controllers/SLAM_controller/SLAM_controller.py
+++ controllers/SLAM_controller/SLAM_controller.py
@@ -47,7 +47,8 @@ distance_gain = 0.3
 
 MAX_VEL_REDUCTION = 0.25
 EPUCK_MAX_WHEEL_SPEED = 0.125 * MAX_VEL_REDUCTION # m/s
-EPUCK_AXLE_DIAMETER = 0.053 # ePuck's wheels are 53mm apart.
+EPUCK_AXLE_DIAMETER = 0.053
+EPUCK_WHEEL_RADIUS = 0.0205 # ePuck's wheels are 0.041m in diameter.
 
 # Index into ground_sensors and ground_sensor_readings for each of the 3 onboard sensors.
 LEFT_IDX = 0
@@ -101,7 +102,7 @@ n = 50 # number of static landmarks
 mu = []
 mu_new = []
 cov = []
-c_prob = []
+#c_prob = []
 
 def populate_map(m):
     obs_list = SLAM_controller_supervisor.supervisor_get_obstacle_positions()
@@ -219,12 +220,12 @@ def get_wheel_speeds(target_pose):
     right_wheel_direction = right_speed_pct * MAX_VEL_REDUCTION
     phi_r_pct = right_speed_pct * MAX_VEL_REDUCTION * rightMotor.getMaxVelocity()
        
-    return phi_l_pct, phi_r_pct
+    return phi_l_pct, phi_r_pct, (phi_l, phi_r)
 
 def EKF_init(x_init):
-    global Rt, Qt, mu, mu_new, cov, c_prob
+    global Rt, Qt, mu, mu_new, cov#, c_prob
 
-    Rt = 5*np.array([[0.1,0,0],
+    Rt = 5*np.array([[0.01,0,0],
                [0,0.01,0],
                [0,0,0.01]])
     Qt = np.array([[0.01,0],
@@ -233,16 +234,21 @@ def EKF_init(x_init):
     mu = np.append(np.array([x_init]).T,np.zeros((2*n,1)),axis=0)
     mu_new = mu
 
-    cov = float("inf")*np.eye(2*n+3)
-    cov[:3,:3] = np.zeros((3,3))
+    cov = 1e6*np.eye(2*n+3)
+    #Init tobot to [0,0,0]
+    #cov[:3,:3] = np.zeros((3,3))
 
-    c_prob = 0.5*np.ones((n,1))
+    #Init robot to known truth pos
+    cov[:3,:3] = np.eye(3,3)*np.array(x_init).T
+
+    #c_prob = 0.5*np.ones((n,1))
 
 def EKF_predict(u, Rt):
+    global mu
     n = len(mu)
 
     # Define motion model f(mu,u)
-    [dtrans, drot1, drot2] = u
+    [dtrans, drot1, drot2] = u # v_t/w_t, delta_t
     motion = np.array([[dtrans*np.cos(mu[2][0]+drot1)],
                        [dtrans*np.sin(mu[2][0]+drot1)],
                        [drot1 + drot2]])
@@ -264,52 +270,52 @@ def EKF_predict(u, Rt):
     return mu_bar, cov_bar
 
 
-def EKF_update(obs, c_prob, Qt):
+def EKF_update(obs, Qt):
     global cov, mu
     N = len(mu)
     
     for [r, theta, j] in obs:
         j = int(j)
-        # if landmark has not been observed before
+        #Landmark has not been observed before
         if cov[2*j+3][2*j+3] >= 1e6 and cov[2*j+4][2*j+4] >= 1e6:
             # define landmark estimate as current measurement
             mu[2*j+3][0] = mu[0][0] + r*np.cos(theta+mu[2][0])
             mu[2*j+4][0] = mu[1][0] + r*np.sin(theta+mu[2][0])
 
+        #Landmark has been seen before, use past values
+        # compute expected observation
+        delta = np.array([mu[2*j+3][0] - mu[0][0], mu[2*j+4][0] - mu[1][0]])
+        q = delta.T.dot(delta)
+        sq = np.sqrt(q)
+        z_theta = np.arctan2(delta[1],delta[0])
+        z_hat = np.array([[sq], [z_theta-mu[2][0]]])
         
-        # if landmark is static
-        if c_prob[j] >= 0.5:
-            # compute expected observation
-            delta = np.array([mu[2*j+3][0] - mu[0][0], mu[2*j+4][0] - mu[1][0]])
-            q = delta.T.dot(delta)
-            sq = np.sqrt(q)
-            z_theta = np.arctan2(delta[1],delta[0])
-            z_hat = np.array([[sq], [z_theta-mu[2][0]]])
-            
-            # calculate Jacobian
-            F = np.zeros((5,N))
-            F[:3,:3] = np.eye(3)
-            F[3,2*j+3] = 1
-            F[4,2*j+4] = 1
-            H_z = np.array([[-sq*delta[0], -sq*delta[1], 0, sq*delta[0], sq*delta[1]],
-                            [delta[1], -delta[0], -q, -delta[1], delta[0]]], dtype='float')
-            H = 1/q*H_z.dot(F)
-    
-            # calculate Kalman gain        
-            K = cov.dot(H.T).dot(np.linalg.inv(H.dot(cov).dot(H.T)+Qt))
-            
-            # calculate difference between expected and real observation
-            z_dif = np.array([[r],[theta]])-z_hat
-            z_dif = (z_dif + np.pi) % (2*np.pi) - np.pi
-            
-            # update state vector and covariance matrix        
-            mu = mu + K.dot(z_dif)
-            cov = (np.eye(N)-K.dot(H)).dot(cov)
+        # calculate Jacobian
+        F = np.zeros((5,N))
+        F[:3,:3] = np.eye(3)
+        F[3,2*j+3] = 1
+        F[4,2*j+4] = 1
+        H_low = np.array([[-sq*delta[0], -sq*delta[1], 0, sq*delta[0], sq*delta[1]],
+                        [delta[1], -delta[0], -q, -delta[1], delta[0]]], dtype='float')
+        H = 1/q*H_low.dot(F)
+
+        # calculate Kalman gain        
+        K = cov.dot(H.T).dot(np.linalg.inv(H.dot(cov).dot(H.T)+Qt))
+        
+        # calculate difference between expected and real observation
+        z_dif = np.array([[r],[theta]])-z_hat
+        z_dif = (z_dif + np.pi) % (2*np.pi) - np.pi
+        
+        # update state vector and covariance matrix        
+        mu = mu + K.dot(z_dif)
+        cov = (np.eye(N)-K.dot(H)).dot(cov)
     
     print('Updated location\t x: {0:.2f} \t y: {1:.2f} \t theta: {2:.2f}'.format(mu[0][0],mu[1][0],mu[2][0]))
     return mu, cov
 
-def move(u):
+def move(target_pose):
+    global mu
+    '''
     motion_noise = np.matmul(np.random.randn(1,3), Rt)[0]
     [dtrans, drot1, drot2] = u[:3] + motion_noise
     
@@ -319,16 +325,28 @@ def move(u):
     theta_new = (x[2] + drot1 + drot2 + np.pi) % (2*np.pi) - np.pi
     
     x_true = [x_new, y_new, theta_new]
-
-    lspeed, rspeed = get_wheel_speeds(x_true)
+    '''
+    lspeed, rspeed, (phi_l, phi_r) = get_wheel_speeds(target_pose)
+    dtrans = np.linalg.norm(target_pose[:2] - np.array(mu[0][0],mu[1][0]))
+    u = [dtrans, phi_l, phi_r]
+    
     print("lspeed: ", lspeed, "rspeed: ", rspeed)
     leftMotor.setVelocity(lspeed)
     rightMotor.setVelocity(rspeed)
 
-def sense(lt):
+    return u
+
+def generate_obs(lt):
     lidar_data = lidar.getRangeImage()
     print(lidar_data)
-    #update_map(lidar_data)
+
+
+    #Run RANSAC on lidar_data
+
+
+    #Itterate through all landmarks, determine which is most likely based on location
+
+    return obs #[r, theta, j]
 
 def update_odometry(left_wheel_direction, right_wheel_direction, time_elapsed):
     '''
@@ -347,6 +365,7 @@ def update_odometry(left_wheel_direction, right_wheel_direction, time_elapsed):
 def main():
     global robot, ground_sensors, ground_sensor_readings, pose_x, pose_y, pose_theta
     global leftMotor, rightMotor, SIM_TIMESTEP, WHEEL_FORWARD, WHEEL_STOPPED, WHEEL_BACKWARDS
+    global cov, cov_new, Rt, Qt, mu, mu_new
     
     last_odometry_update_time = None
     
@@ -357,17 +376,18 @@ def main():
     # Sensor burn-in period
     for i in range(10): robot.step(SIM_TIMESTEP)
 
+    #Known truth start pose
     start_pose = SLAM_controller_supervisor.supervisor_get_robot_pose()
     pose_x, pose_y, pose_theta = start_pose
 
-    #Init
+    lidar_obs = []
+    u = [0, 0, 0] #State vector
+    #Init with known start location
     EKF_init(start_pose)
 
     print("MU: ", mu)
 
     print("Cov: ", cov)
-
-    print("c_prob: ", c_prob)
 
     # Main Control Loop:
     while robot.step(SIM_TIMESTEP) != -1:
@@ -382,24 +402,28 @@ def main():
         update_odometry(left_wheel_direction, right_wheel_direction, time_elapsed)
         last_odometry_update_time = robot.getTime()
         print("Current pose: [%5f, %5f, %5f]" % (pose_x, pose_y, pose_theta))
-
+        state = "move"
         if state == "":
             pass
         #Move
         elif state == "move":
-            move(u)
+            #Move and update state vector for EKF
+            new_pose = [0.1, -0.24, 0]
+            u = move(new_pose)
+            print("U: ", u)
         #Sense
         elif state == "sense":
-            sense()
+            lidar_obs = generate_obs
+
         #Predict
         elif state == "predict":
             mu_new, cov = EKF_predict(u, Rt)
             mu = np.append(mu,mu_new,axis=1)
         #Update
         elif state == "update":
-            mu_new, cov, c_prob_new = EKF_update(lidar_obs, c_prob[:,-1].reshape(n,1), Qt)
+            mu_new, cov = EKF_update(lidar_obs, Qt)#EKF_update(lidar_obs, c_prob[:,-1].reshape(n,1), Qt)
             mu = np.append(mu,mu_new,axis=1)
-            c_prob = np.append(c_prob, c_prob_new, axis=1)
+            #c_prob = np.append(c_prob, c_prob_new, axis=1)
         else:
             #Stop
             print("Stopping!")
