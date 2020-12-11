58680cd14257210434417cfc7fe9919e4a582435
apkedm <deanneapke@gmail.com>
2020-12-11 11:30:06 -0700

    generate_obs runs through extracting the landmarks from lidar

--- controllers/SLAM_controller/SLAM_controller.py
+++ controllers/SLAM_controller/SLAM_controller.py
@@ -171,7 +171,87 @@ def convert_lidar_reading_to_world_coord(lidar_bin, lidar_distance):
     x = math.cos( pose_theta ) * bQ_x - math.sin( pose_theta ) * bQ_y + pose_x
     y = math.sin( pose_theta ) * bQ_x + math.cos( pose_theta ) * bQ_y + pose_y
     #print(x,y)
-    return x, y
+    return [x, y]
+
+
+#From Lab 5
+def transform_world_coord_to_map_coord(world_coord):
+    """
+    @param world_coord: Tuple of (x,y) position in world coordinates
+    @return grid_coord: Tuple of (i,j) coordinates corresponding to grid row (y-coord) and column (x-coord) in our map
+    """
+    col, row = np.array(world_coord) / CELL_RESOLUTIONS
+    if row < 0 or col < 0 or row >= NUM_Y_CELLS or col >= NUM_X_CELLS:
+        return None
+
+    return tuple(np.array([row, col]).astype(int))
+
+
+#From Lab 5
+def transform_map_coord_world_coord(map_coord):
+    """
+    @param map_coord: Tuple of (i,j) coordinates corresponding to grid column and row in our map
+    @return world_coord: Tuple of (x,y) position corresponding to the center of map_coord, in world coordinates
+    """
+    row, col = map_coord
+    if row < 0 or col < 0 or row >= NUM_Y_CELLS or col >= NUM_X_CELLS:
+        return None
+    
+    return np.array([(col+0.5)*CELL_RESOLUTIONS[1], (row+0.5)*CELL_RESOLUTIONS[0]])
+
+
+#From Lab 5
+def get_wheel_speeds(target_pose):
+    '''
+    @param target_pose: Array of (x,y,theta) for the destination robot pose
+    @return motor speed as percentage of maximum for left and right wheel motors
+    '''
+    global pose_x, pose_y, pose_theta, left_wheel_direction, right_wheel_direction
+
+    pose_x, pose_y, pose_theta = SLAM_controller_supervisor.supervisor_get_robot_pose()
+
+
+    bearing_error = math.atan2( (target_pose[1] - pose_y), (target_pose[0] - pose_x) ) - pose_theta
+    distance_error = np.linalg.norm(target_pose[:2] - np.array([pose_x,pose_y]))
+    heading_error = target_pose[2] -  pose_theta
+
+    BEAR_THRESHOLD = 0.06
+    DIST_THRESHOLD = 0.03
+    dT_gain = theta_gain
+    dX_gain = distance_gain
+    if distance_error > DIST_THRESHOLD:
+        dTheta = bearing_error
+        if abs(bearing_error) > BEAR_THRESHOLD:
+            dX_gain = 0
+    else:
+        dTheta = heading_error
+        dX_gain = 0
+
+    dTheta *= dT_gain
+    dX = dX_gain * min(3.14159, distance_error)
+
+    phi_l = (dX - (dTheta*EPUCK_AXLE_DIAMETER/2.)) / EPUCK_WHEEL_RADIUS
+    phi_r = (dX + (dTheta*EPUCK_AXLE_DIAMETER/2.)) / EPUCK_WHEEL_RADIUS
+
+    left_speed_pct = 0
+    right_speed_pct = 0
+    
+    wheel_rotation_normalizer = max(abs(phi_l), abs(phi_r))
+    left_speed_pct = (phi_l) / wheel_rotation_normalizer
+    right_speed_pct = (phi_r) / wheel_rotation_normalizer
+    
+    if distance_error < 0.05 and abs(heading_error) < 0.05:    
+        left_speed_pct = 0
+        right_speed_pct = 0
+        
+    left_wheel_direction = left_speed_pct * MAX_VEL_REDUCTION
+    phi_l_pct = left_speed_pct * MAX_VEL_REDUCTION * leftMotor.getMaxVelocity()
+
+    right_wheel_direction = right_speed_pct * MAX_VEL_REDUCTION
+    phi_r_pct = right_speed_pct * MAX_VEL_REDUCTION * rightMotor.getMaxVelocity()
+       
+    return phi_l_pct, phi_r_pct, (phi_l, phi_r)
+
 
 def perform_least_squares_line_estimate(lidar_world_coords, selected_points):
     """
@@ -331,84 +411,6 @@ def extract_line_landmarks(lidar_world_coords):
     
     return found_landmarks
 
-
-#From Lab 5
-def transform_world_coord_to_map_coord(world_coord):
-    """
-    @param world_coord: Tuple of (x,y) position in world coordinates
-    @return grid_coord: Tuple of (i,j) coordinates corresponding to grid row (y-coord) and column (x-coord) in our map
-    """
-    col, row = np.array(world_coord) / CELL_RESOLUTIONS
-    if row < 0 or col < 0 or row >= NUM_Y_CELLS or col >= NUM_X_CELLS:
-        return None
-
-    return tuple(np.array([row, col]).astype(int))
-
-#From Lab 5
-def transform_map_coord_world_coord(map_coord):
-    """
-    @param map_coord: Tuple of (i,j) coordinates corresponding to grid column and row in our map
-    @return world_coord: Tuple of (x,y) position corresponding to the center of map_coord, in world coordinates
-    """
-    row, col = map_coord
-    if row < 0 or col < 0 or row >= NUM_Y_CELLS or col >= NUM_X_CELLS:
-        return None
-    
-    return np.array([(col+0.5)*CELL_RESOLUTIONS[1], (row+0.5)*CELL_RESOLUTIONS[0]])
-
-
-#From Lab 5
-def get_wheel_speeds(target_pose):
-    '''
-    @param target_pose: Array of (x,y,theta) for the destination robot pose
-    @return motor speed as percentage of maximum for left and right wheel motors
-    '''
-    global pose_x, pose_y, pose_theta, left_wheel_direction, right_wheel_direction
-
-    pose_x, pose_y, pose_theta = SLAM_controller_supervisor.supervisor_get_robot_pose()
-
-
-    bearing_error = math.atan2( (target_pose[1] - pose_y), (target_pose[0] - pose_x) ) - pose_theta
-    distance_error = np.linalg.norm(target_pose[:2] - np.array([pose_x,pose_y]))
-    heading_error = target_pose[2] -  pose_theta
-
-    BEAR_THRESHOLD = 0.06
-    DIST_THRESHOLD = 0.03
-    dT_gain = theta_gain
-    dX_gain = distance_gain
-    if distance_error > DIST_THRESHOLD:
-        dTheta = bearing_error
-        if abs(bearing_error) > BEAR_THRESHOLD:
-            dX_gain = 0
-    else:
-        dTheta = heading_error
-        dX_gain = 0
-
-    dTheta *= dT_gain
-    dX = dX_gain * min(3.14159, distance_error)
-
-    phi_l = (dX - (dTheta*EPUCK_AXLE_DIAMETER/2.)) / EPUCK_WHEEL_RADIUS
-    phi_r = (dX + (dTheta*EPUCK_AXLE_DIAMETER/2.)) / EPUCK_WHEEL_RADIUS
-
-    left_speed_pct = 0
-    right_speed_pct = 0
-    
-    wheel_rotation_normalizer = max(abs(phi_l), abs(phi_r))
-    left_speed_pct = (phi_l) / wheel_rotation_normalizer
-    right_speed_pct = (phi_r) / wheel_rotation_normalizer
-    
-    if distance_error < 0.05 and abs(heading_error) < 0.05:    
-        left_speed_pct = 0
-        right_speed_pct = 0
-        
-    left_wheel_direction = left_speed_pct * MAX_VEL_REDUCTION
-    phi_l_pct = left_speed_pct * MAX_VEL_REDUCTION * leftMotor.getMaxVelocity()
-
-    right_wheel_direction = right_speed_pct * MAX_VEL_REDUCTION
-    phi_r_pct = right_speed_pct * MAX_VEL_REDUCTION * rightMotor.getMaxVelocity()
-       
-    return phi_l_pct, phi_r_pct, (phi_l, phi_r)
-
 def EKF_init(x_init):
     global Rt, Qt, mu, mu_new, cov#, c_prob
 
@@ -527,10 +529,14 @@ def generate_obs(lt):
     lidar_data = lidar.getRangeImage()
 
     #convert lidar to world locations
-
+    lidar_readings = []
+    for i in range(21):
+        lidar_found_loc = convert_lidar_reading_to_world_coord(i, lidar_data[i])
+        if lidar_found_loc is not None:
+            lidar_readings.append(lidar_found_loc)
 
     #Run RANSAC on lidar_data
-
+    extracted_landmarks = extract_line_landmarks(lidar_readings)
 
     #Itterate through all landmarks, determine which is most likely based on location
 
